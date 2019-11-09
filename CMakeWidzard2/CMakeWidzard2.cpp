5d2124a2d8685e57b746fe9642180f4ec242a005
taehyeong77@naver.com <taehyeong77@naver.com>
2019-11-09 14:47:16 +0900

    cc

--- CMakeWidzard2/CMakeWidzard2.cpp
+++ CMakeWidzard2/CMakeWidzard2.cpp
@@ -4,4 +4,32 @@ CMakeWidzard2::CMakeWidzard2(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	ui.flagsTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+}
+
+void CMakeWidzard2::on_actionExport_triggered()
+{
+	QString projectName = ui.ProjectNamelineEdit->text();
+}
+
+
+void CMakeWidzard2::InsertFlagItem(const QString& key, const QString& value)
+{
+	int row = ui.flagsTable->rowCount();
+	ui.flagsTable->insertRow(row);
+	auto KeyColumn = new QTableWidgetItem(key);
+	auto valueColumn = new QTableWidgetItem(value);
+	ui.flagsTable->setItem(row,0,KeyColumn);
+	ui.flagsTable->setItem(row, 0, valueColumn);
+}
+
+void CMakeWidzard2::on_AddBtn_clicked()
+{
+	InsertFlagItem("<key>", "<value>");
+}
+
+void CMakeWidzard2::on_RemoveBtn_clicked()
+{
+	ui.flagsTable->removeRow(ui.flagsTable->currentRow());
 }
--- CMakeWidzard2/CMakeWidzard2.h
+++ CMakeWidzard2/CMakeWidzard2.h
@@ -10,6 +10,13 @@ class CMakeWidzard2 : public QMainWindow
 public:
 	CMakeWidzard2(QWidget *parent = Q_NULLPTR);
 
+	void InsertFlagItem(const QString&, const QString&);
+
+private slots:
+	void on_actionExport_triggered();
+	void on_AddBtn_clicked();
+	void on_RemoveBtn_clicked();
+
 private:
 	Ui::CMakeWidzard2Class ui;
 };
